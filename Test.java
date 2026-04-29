1507cb04b1f5bc0a888d2d963e0b04d13bb2a030
bemnetzolabura <bemnetzolabura@gmail.com>
2026-04-29 19:38:30 +0300

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your score: ");
+        double score = input.nextDouble(); // 👈 accepts decimals
+
+        if (score < 0 || score > 100) {
+            System.out.println("Invalid score");
+        } else if (score >= 80) {
+            System.out.println("Excellent");
+        } else if (score >= 60) {
+            System.out.println("Good");
+        } else if (score >= 50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+
+        input.close();
+    }
+}
